e5c16e071e9ab1ef04f2a178187672a4270dd398
Austin <Austin@DESKTOP-QTJ1OTQ>
2017-02-09 01:39:25 -0500

    Implemented the clean-blog theme; navbar not showing

--- MVCBlog/App_Start/BundleConfig.cs
+++ MVCBlog/App_Start/BundleConfig.cs
@@ -21,10 +21,14 @@ namespace MVCBlog
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
                       "~/Scripts/bootstrap.js",
+                      "~/Scripts/clean-blog.js",
+                      "~/Scripts/jqBootstrapValidation.js",
+                      "~/Scripts/contact_me.js",
                       "~/Scripts/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
+                      "~/Content/bootstrap.min.css",
+                      "~/Content/clean-blog.min.css",
                       "~/Content/site.css"));
         }
     }
